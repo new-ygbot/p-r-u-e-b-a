08133d08de0da5059ef11120b38caaaae79d21a2
new-ygbot <110633406+new-ygbot@users.noreply.github.com>
2022-08-09 22:21:20 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-admin = 'rockstar984'
-bot_token = "5578817560:AAFthnpCr1QC0_wlAObLZmubDjDDdyPwQII"
+admin = 'nuevoss01'
+bot_token = "5548109301:AAFSo15CQF6JdF3X2cr6Iu_kRyVwR4Y8QTo"
 
-groupid= -1001728956224
+groupid= -684078191
